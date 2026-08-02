4b0b71cb590543d2a2ced0d6523cf55212cb3f53
raj-coder-prog <yourbusiness368@gmail.com>
2026-08-02 10:58:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -259,4 +259,5 @@ def download_video():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True, host='127.0.0.1', port=5000, threaded=True)
+    port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
